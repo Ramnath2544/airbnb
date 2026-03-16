12671de05c9aa3498da77e1ea48ff4585f6f84be
Ramnath <ramnath2544@gmail.com>
2026-03-16 18:57:07 +0530

    Initial commit

---
+++ DB/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/**/*.{html,ejs}",
+    "./views/partials/**/*.{html,ejs}",
+    "./routes/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
